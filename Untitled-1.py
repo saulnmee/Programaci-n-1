f9e31932bbe888838dcd072be9bd074ba07069a0
saulnmee <veguesau@gmail.com>
2025-11-04 19:59:31 -0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+
+print("Calculadora para rectangulos")
+
+def calcular_area_rectangulo(b,h):
+    area = (b * h)
+    return b * h
+
+area = calcular_area_rectangulo(10,5)
+
+print(f"El area es: (area)")
+print("El area del rectangulo es:(area)")
